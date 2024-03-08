d2533e985573fde99219dd612a130c02a93fe196
Hyphenhypen <adityapratapsjnhh7654@gmail.com>
2024-03-08 23:44:04 +0530

    feat: auth done

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 const connectToDB = require("./db/connect.js");
 const authTeacherRoutes = require("./routes/authTeacher.routes.js");
+const authStudentRoutes = require("./routes/authStudent.routes.js");
 const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
@@ -14,7 +15,8 @@ app.get('/', (req, res) => {
     res.send("Hello World");
 })
 
-app.use('/api/auth/teacher/', authTeacherRoutes); 
+app.use('/api/auth/teacher/', authTeacherRoutes);
+app.use('/api/auth/student/', authStudentRoutes);
 
 app.listen(port, ()=>{
     connectToDB();
